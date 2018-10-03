dc012cec3abb8a5df9c5a08c498d84392a3a54de
Jakub Mifek <jakubmifek@gmail.com>
2018-10-03 16:39:10 +0200

    Create homework #01.pl

---
+++ Constraint Programming/homework #01.pl
@@ -0,0 +1,26 @@
+man(jacob).
+man(jerry).
+man(michael).
+man(arnold).
+man(thomas).
+
+woman(ivet).
+woman(victoria).
+woman(jane).
+woman(denise).
+woman(katherine).
+
+parent(jacob, jerry).
+parent(jacob, jane).
+parent(victoria, jerry).
+parent(victoria, jane).
+
+parent(jerry, denise).
+parent(ivet, denise).
+parent(jerry, michael).
+parent(ivet, michael).
+
+parent(thomas, arnold).
+parent(jane, arnold).
+parent(thomas, catherine).
+parent(jane, catherine).
