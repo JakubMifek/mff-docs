ab7e95c694c2ef8c27f2f712f23af5c0c1264c7c
Jakub Mifek <jakubmifek@gmail.com>
2018-10-03 17:15:20 +0200

    Update homework #01 - genealogy.pl

--- Constraint Programming/homework #01 - genealogy.pl
+++ Constraint Programming/homework #01 - genealogy.pl
@@ -24,3 +24,19 @@ parent(thomas, arnold).
 parent(jane, arnold).
 parent(thomas, catherine).
 parent(jane, catherine).
+
+father(X,Y):-man(X),parent(X,Y).
+mother(X,Y):-woman(X),parent(X,Y).
+
+son(X,Y):-man(X),parent(Y,X).
+daughter(X,Y):-woman(X),parent(Y,X).
+
+grandparent(X,Y):-parent(X,Z),parent(Z,Y).
+
+siblings(X,Y):-parent(Z,X),parent(Z,Y),X\=Y.
+
+uncle(X,Y):-man(X),siblings(X,Z),parent(Z,Y).
+aunt(X,Y):-woman(X),parent(Z,Y),siblings(X,Z).
+
+descendant(X,Y):-parent(Y,X).
+descendant(X,Y):-parent(Y,Z),descendant(X,Z).
